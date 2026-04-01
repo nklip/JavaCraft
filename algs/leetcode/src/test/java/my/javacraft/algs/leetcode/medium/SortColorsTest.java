808a97a66b45b2fbf083688009472c40b110aeda
nikita.lipatov <nikita.lipatov@gmail.com>
2026-04-01 02:13:18 +0100

    add SortColors

---
+++ algs/leetcode/src/test/java/my/javacraft/algs/leetcode/medium/SortColorsTest.java
@@ -0,0 +1,29 @@
+package my.javacraft.algs.leetcode.medium;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SortColorsTest {
+
+    @Test
+    void testCase1() {
+        int[] actualNums = new int[]{2,0,2,1,1,0};
+        SortColors solution = new SortColors();
+
+        solution.sortColors(actualNums);
+
+        int[] expectedNums = new int[]{0,0,1,1,2,2};
+        Assertions.assertArrayEquals(expectedNums, actualNums);
+    }
+
+    @Test
+    void testCase2() {
+        int[] actualNums = new int[]{2,0,1};
+        SortColors solution = new SortColors();
+
+        solution.sortColors(actualNums);
+
+        int[] expectedNums = new int[]{0,1,2};
+        Assertions.assertArrayEquals(expectedNums, actualNums);
+    }
+}
