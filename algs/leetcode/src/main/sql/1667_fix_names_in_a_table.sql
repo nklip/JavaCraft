7c91be3a7c85dfdb03ebc2ffc4c5510fb044e9b8
nikita.lipatov <nikita.lipatov@gmail.com>
2026-06-10 02:15:59 +0100

    add 1667, 1683, 1693, 1729, 1731, 1741, 1757, 1789 sql leetcode problems

---
+++ algs/leetcode/src/main/sql/1667_fix_names_in_a_table.sql
@@ -0,0 +1,44 @@
+-- 1667. Fix Names in a Table
+--
+-- LeetCode: https://leetcode.com/problems/fix-names-in-a-table/description/
+--
+-- Table: Users
+--
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | user_id        | int     |
+-- | name           | varchar |
+-- +----------------+---------+
+-- user_id is the primary key (column with unique values) for this table.
+-- This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+--
+-- Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+--
+-- Return the result table ordered by user_id.
+--
+-- The result format is in the following example.
+--
+-- Example 1:
+--
+-- Input:
+-- Users table:
+-- +---------+-------+
+-- | user_id | name  |
+-- +---------+-------+
+-- | 1       | aLice |
+-- | 2       | bOB   |
+-- +---------+-------+
+--
+-- Output:
+-- +---------+-------+
+-- | user_id | name  |
+-- +---------+-------+
+-- | 1       | Alice |
+-- | 2       | Bob   |
+-- +---------+-------+
+
+-- INITCAP doesn't work here as we need to capitalize ONLY the first char
+SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) as name
+FROM Users
+ORDER BY user_id;
